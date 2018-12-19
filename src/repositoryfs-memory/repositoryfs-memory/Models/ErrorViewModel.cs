933d8f66e00c602f3f2b5dd99b14e2309f957182
dstrauss <fmdenver@gmail.com>
2018-12-18 21:51:02 -0500

    Initial Commit

---
+++ src/repositoryfs-memory/repositoryfs-memory/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace repositoryfs_memory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
